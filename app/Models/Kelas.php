95e921e955e56e188334c4b76c3b6127f006eb32
= <nexrey19@gmail.com>
2021-04-10 17:07:14 +0700

    Model fixing

--- app/Models/Kelas.php
+++ app/Models/Kelas.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Mahasiswa;
+use App\Models\Kelas;
+
 
 class Kelas extends Model
 {
--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,24 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Mahasiswa;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\Mahasiswa as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model; //Model Eloquent
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
     protected $table = 'Mahasiswa';
-    public $timestamps= false; 
     protected $primaryKey = 'nim';
 
     protected $fillable = [
-        'Nim',
-        'Nama',
+        'nim',
+        'nama',
         'kelas_id',
-        'Jurusan',
+        'jurusan',
         ];
 
     public function kelas(){
