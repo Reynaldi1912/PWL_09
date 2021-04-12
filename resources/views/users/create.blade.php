149b5cb29f9324742de123c81a83efe57214920e
= <nexrey19@gmail.com>
2021-04-12 18:47:52 +0700

    create view fix

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -32,7 +32,7 @@
  </div>
  <div class="form-group">
     <label for="Kelas">Kelas</label>
-    <select class="form-control">
+    <select name="Kelas_Id" class="form-control">
         @foreach($kelas as $kls)
             <option value="{{$kls->id}}">{{$kls->nama_kelas}}</option>
         @endforeach
